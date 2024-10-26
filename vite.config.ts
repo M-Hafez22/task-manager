48d1ea9a40f358d658231ee086fa8fe16a9a03ad
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-10-26 17:38:48 +0300

    Setup for test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   base: "/task-manager/",
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom", // Mock DOM for component testing
+    setupFiles: "./src/setupTests.ts", // Optional: setup file
+  },
 })
