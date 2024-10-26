6e40c33cb2bc6effc93245c7c07d1d397d1dd408
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-10-26 12:40:04 +0300

    Feat: Show Image

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -8,6 +8,13 @@ interface TaskCardProps {
 
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => (
   <Card className="p-4">
+    {task.image && (
+      <img
+        src={task.image}
+        alt={task.title}
+        className="w-full h-32 object-cover mb-4"
+      />
+    )}
     <h3 className="font-semibold">{task.title}</h3>
     <p>{task.description}</p>
     <p>Priority: {task.priority}</p>
